f5abdde796cd291241dd3226560ba505507e1783
kelvinchanwh <35751887+kelvinchanwh@users.noreply.github.com>
2020-11-08 15:53:35 +0800

    Changed API to https://rss2json.com Added recursive rss functionality

--- rss.js
+++ rss.js
@@ -1,55 +1,47 @@
-const DOMPARSER = new DOMParser().parseFromString.bind(new DOMParser());
-
-// the actual URL's of the RSS feed
-var feedUrls = [
-	"https://hacks.mozilla.org/feed/", 
-	"https://webkit.org/feed/"
-];
-
 var frag = document.createDocumentFragment();
 var hasBegun = true;
 
-feedUrls.forEach((u) => {
-	try {
-		var url = new URL(u);
+try {
+	urlParams = new URLSearchParams(window.location.search);
+	if (window.location.href.indexOf('?' + 'url' + '=') != -1) {
+		url = urlParams.get('url');
+	} else {
+		url = "http://allrss.se/dramas/";
 	}
-	catch (e) {
-		console.error('URL invalid: ' + u);
-		return;
-	}
+	console.log(url)
+}
+catch (e) {
+	console.error('URL invalid');
+}
 
-	// use Yahoo's YQL service to circumvent CORS
-	// https://developer.yahoo.com/yql/
-	var yql = 'select * from rss where url="' + url + '"';
-	var yqlUrl = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(yql);
+// use https://rss2json.com/ to circumvent CORS
+var yqlUrl = 'https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(url);
 
-	// fetch the RSS Feed
-	fetch(yqlUrl).then((res) => {
-		res.text().then((xmlTxt) => {
-			// parse the RSS Feed and display the content
-			try {
-				let doc = DOMPARSER(xmlTxt, "text/xml");
-				let heading = document.createElement('h1');
-				heading.textContent = url.hostname;
-				frag.appendChild(heading);
-				doc.querySelectorAll('item').forEach((item) => {
-					let temp = document.importNode(document.querySelector('template').content, true);
-					let i = item.querySelector.bind(item);
-					let t = temp.querySelector.bind(temp);
-					t('h2').textContent = !!i('title') ? i('title').textContent : '-';
-					t('a').textContent = t('a').href = !!i('link') ? i('link').textContent : '#';
-					t('p').innerHTML = !!i('description') ? i('description').textContent : '-';
-					t('h3').textContent = url.hostname;
-					frag.appendChild(temp);
-				})
-			} catch (e) {
-				console.error('Error in parsing the feed');
-			}
-			if (hasBegun) {
-				document.querySelector('output').textContent = ''; 
-				hasBegun = false;
-			}
-			document.querySelector('output').appendChild(frag);
-		});
-	}).catch(() => console.error('Error in fetching the RSS feed'));
-});
+// fetch the RSS Feed
+fetch(yqlUrl).then((res) => {
+	res.text().then((jsonTxt) => {
+		// parse the RSS Feed and display the content
+		try {
+			let heading = document.createElement('h1');
+			heading.textContent = url.hostname;
+			frag.appendChild(heading);
+			var t = JSON.parse(jsonTxt);
+			t["items"].forEach((item) => {
+				let temp = document.importNode(document.querySelector('template').content,true);
+				let tempdoc = temp.querySelector.bind(temp);
+				tempdoc('h2').textContent = item.title;
+				tempdoc('a').textContent = item["enclosure"]['link']
+				tempdoc('a').href = (window.location.href.split('?')[0] + '?url=' + item["enclosure"]['link']);
+				//tempdoc('img').href = item["description"].split("\"")[1]
+				frag.appendChild(temp);
+			})
+		} catch (e) {
+			console.error('Error in parsing the feed');
+		}
+		if (hasBegun) {
+			document.querySelector('output').textContent = ''; 
+			hasBegun = false;
+		}
+		document.querySelector('output').appendChild(frag);
+	});
+}).catch(() => console.error('Error in fetching the RSS feed'));
